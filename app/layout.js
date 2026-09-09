75427fe65ad7278f8fcb15bd4b9ec2037bb8b88b
Michael de Jong <dej.michael@hotmail.com>
2026-09-09 15:26:27 +0200

    added some basic structure and components

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,46 @@
+function Navigation() {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="#about">About</a>
+      </li>
+      <li>
+        <a href="#services">Services</a>
+      </li>
+      <li>
+        <a href="#contact">Contact</a>
+      </li>
+    </ul>
+  );
+}
+
+function Hero() {
+  return <div className="header-hero">[HERO IMAGE HERE]</div>;
+}
+
+function Cta() {
+  return <div className="header-cta">Bel Ons: +6 452536985</div>;
+}
+
 export default function RootLayout({ children }) {
   return (
     <html lang="nl">
       <body>
-        <main>{children}</main>
+        <header>
+          <div className="header-logo">
+            <a href="#">Logo</a>
+          </div>
+          <nav>
+            <Navigation />
+          </nav>
+          <Cta />
+          <Hero />
+        </header>
+        {children}
+        <footer>Hier komt de footer</footer>
       </body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,7 +1,17 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Titel</h1>
-    </div>
+    <main>
+      <div id="about">
+        <h2>About</h2>
+      </div>
+
+      <div id="services">
+        <h2>Services</h2>
+      </div>
+
+      <div id="contact">
+        <h2>Contact</h2>
+      </div>
+    </main>
   );
 }
