eef8286bec915ffadcf14fad3ee744266f45fe92
Michael de Jong <dej.michael@hotmail.com>
2026-09-24 16:48:46 +0200

    added basic styling and new logo

--- app/layout.js
+++ app/layout.js
@@ -4,14 +4,19 @@ import Image from "next/image";
 function Navigation() {
   return (
     <nav
-      className="navbar is-fixed-top"
+      className="navbar is-fixed-top py-3"
       role="navigation"
       aria-label="main navigation"
     >
       <div className="container">
         <div className="navbar-brand">
           <a href="/" className="navbar-item">
-            [Logo]
+            <Image
+              src="/images/gvr-logo.jpg"
+              alt="Logo"
+              width="200"
+              height="91"
+            ></Image>
           </a>
           <a
             className="navbar-burger"
@@ -42,12 +47,26 @@ function Navigation() {
           </div>
           <div className="navbar-end">
             <div className="navbar-item">
-              <div className="buttons">
-                <a className="button is-warning" href="tel:0768945325">
-                  <strong>BEL ONS:</strong>
-                  0768945325
-                </a>
-              </div>
+              <a
+                className="call-button"
+                href="tel:+31768945325"
+                aria-label="Bel ons: 076 894 5325"
+              >
+                <span className="call-button__icon" aria-hidden="true">
+                  <svg
+                    viewBox="0 0 24 24"
+                    width="18"
+                    height="18"
+                    fill="currentColor"
+                  >
+                    <path d="M6.6 10.8a15.1 15.1 0 0 0 6.6 6.6l2.2-2.2c.3-.3.7-.4 1-.2 1.1.4 2.3.6 3.6.6.6 0 1 .4 1 1V20c0 .6-.4 1-1 1A17 17 0 0 1 3 4c0-.6.4-1 1-1h3.5c.6 0 1 .4 1 1 0 1.3.2 2.5.6 3.6.1.3 0 .7-.2 1l-2.3 2.2z" />
+                  </svg>
+                </span>
+                <span className="call-button__text">
+                  <small>Bel ons direct</small>
+                  <strong>076 894 5325</strong>
+                </span>
+              </a>
             </div>
           </div>
         </div>
